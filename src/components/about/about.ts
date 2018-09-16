1176547cefccb34ef6c5700cbba16deaae5411bc
Serhii Shkliarskiy <shkliarskiy@gmail.com>
2018-09-16 23:28:06 +0300

    done

--- src/components/about/about.ts
+++ src/components/about/about.ts
@@ -11,17 +11,31 @@ export class AboutComponent {
 
   constructor(private appRate: AppRate) {
 
+/*
     this.appRate.preferences.storeAppURL = {
       android: 'market://details?id=ek.knteu.timetable'
     };
 
     this.appRate.preferences.useLanguage = 'uk-UK'
+*/
+
+this.appRate.preferences = {
+  usesUntilPrompt: 1,
+  storeAppURL: {
+    android: 'market://details?id=ek.knteu.timetable'
+  },
+  useLanguage: 'uk-UK',
+  displayAppName: 'розклад КНТЕУ'
+}
+
+
   }
 
 
 
   goToPlayMarket() {
-    this.appRate.promptForRating(true);
+    // this.appRate.promptForRating(true);
+    this.appRate.navigateToAppStore();
   }
 }
 
